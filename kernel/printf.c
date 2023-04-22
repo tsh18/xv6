d1c03f0f36893382e012186aecc4248590dfd607
tsh <tsh@qq.com>
2023-04-22 12:08:15 +0800

    Backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -133,3 +133,14 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void backtrace(void) {
+  printf("backtrace:\n");
+
+  uint64 fp = r_fp();
+
+  while (fp != PGROUNDDOWN(fp)) { // 不在同一页说明已经到达栈底
+    printf("%p\n", *((uint64 *)(fp - 8)));
+    fp = *((uint64 *)(fp - 16));
+  }
+}
